4ca02ab622c3ea7540b4ff2c4cb88084e6a77bc7
Shashikant86 <shashikant.jagtap@icloud.com>
2017-08-01 22:21:10 +0100

    Adding some chat to test

--- gatling_jenkins.sh
+++ gatling_jenkins.sh
@@ -4,13 +4,22 @@ GATLING_VERSION=2.2.5
 cd ${WORKSPACE}
 
 # Remove old Gatling reports and version Download New
-
+echo "======== Remove old Gatling Snapshot if it's there  ========== "
+echo "*********************************\n "
 rm -rf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION
 
-
+echo "======== Downloading Gatling Framework from Web ========== "
+echo "*********************************\n "
 curl -sf -o ${WORKSPACE}/gatling-$GATLING_VERSION.zip \
   -L https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/$GATLING_VERSION/gatling-charts-highcharts-bundle-$GATLING_VERSION-bundle.zip
 ls
+
+echo "======== Gatling Zip successfully downloaded ========== "
+echo "*********************************\n "
+
+echo "======== Unziping Gatling. Please be patient as it wil take some tme depending on network ========== "
+echo "*********************************\n "
+
 unzip ${WORKSPACE}/gatling-$GATLING_VERSION.zip
 
 chmod +x ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/bin/gatling.sh
@@ -20,11 +29,16 @@ export GATLING_HOME=${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSI
 
 # Remove default simulations & Config and add our own
 
+echo "======== Removing the default Simulations and replacing with our own \n ========== "
+echo "*********************************\n "
+
 rm -rf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/user-files/simulations/
 rm -rf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/conf
 cp ${WORKSPACE}/user-files/ ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/user-files/
 cp ${WORKSPACE}/conf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/conf/
 
 # RUN Gatling TEST WITH CLASS CLASS/TEST SCENARIO SPECIFIED
+echo "======== Running Gatling Performance/Load Test \n ========== "
+echo "*********************************\n "
 
 sh ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/bin/gatling.sh -sf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/user-files/simulations/ -s SampleLoadTEST -rf ${WORKSPACE}/gatling-charts-highcharts-bundle-$GATLING_VERSION/results/
--- gatling_local.sh
+++ gatling_local.sh
@@ -2,14 +2,21 @@
 GATLING_VERSION=2.2.5
 
 # Remove old Gatling reports and version Download New
-
+echo "======== Remove old Gatling Snapshot if it's there  ========== "
+echo "*********************************\n "
 rm -rf gatling-charts-highcharts-bundle-${GATLING_VERSION}/
 rm -rf gatling-${GATLING_VERSION}.zip/
 
-
+echo "======== Downloading Gatling Framework from Web ========== "
+echo "*********************************\n "
 curl -sf -o gatling-${GATLING_VERSION}.zip \
   -L https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/${GATLING_VERSION}/gatling-charts-highcharts-bundle-${GATLING_VERSION}-bundle.zip
 ls
+echo "======== Gatling Zip successfully downloaded ========== "
+echo "*********************************\n "
+
+echo "======== Unziping Gatling. Please be patient as it wil take some tme depending on network ========== "
+echo "*********************************\n "
 unzip gatling-${GATLING_VERSION}.zip
 
 chmod +x gatling-charts-highcharts-bundle-${GATLING_VERSION}/bin/gatling.sh
@@ -19,6 +26,9 @@ export GATLING_HOME=gatling-charts-highcharts-bundle-${GATLING_VERSION}
 
 # Remove default simulations and Config and add our own
 
+echo "======== Removing the default Simulations and replacing with our own \n ========== "
+echo "*********************************\n "
+
 rm -rf gatling-charts-highcharts-bundle-${GATLING_VERSION}/user-files/
 rm -rf gatling-charts-highcharts-bundle-${GATLING_VERSION}/conf
 cp -rf user-files/ gatling-charts-highcharts-bundle-${GATLING_VERSION}/user-files/
@@ -26,4 +36,6 @@ cp -rf config/ gatling-charts-highcharts-bundle-${GATLING_VERSION}/config
 
 # RUN Gatling TEST WITH CLASS CLASS/TEST SCENARIO SPECIFIED
 
+echo "======== Running Gatling Performance/Load Test \n ========== "
+echo "*********************************\n "
 sh gatling-charts-highcharts-bundle-${GATLING_VERSION}/bin/gatling.sh -sf gatling-charts-highcharts-bundle-${GATLING_VERSION}/user-files/simulations/ -s SampleLoadTest -rf gatling-charts-highcharts-bundle-${GATLING_VERSION}/results/
